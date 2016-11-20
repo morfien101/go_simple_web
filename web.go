56e41bd4ef791334a22e495ec633bdf1b248e97e
Randy Coburn <morfien101@gmail.com>
2016-11-20 08:43:40 +0000

    added a log line that will go to stdout. This can be scraped by a container logger. Also added a version number.

--- web.go
+++ web.go
@@ -5,14 +5,18 @@ import(
 	"net/http"
 	"io"
 	"fmt"
+	"time"
 )
+
+const VERSION = "0.0.1"
 
 func responder(w http.ResponseWriter, r *http.Request) {
 	hostname,_ := os.Hostname()
-	io.WriteString(w, fmt.Sprintf("<html><head><title>%s</title></head><body><h1>You hit: %s</h1></body></html>",hostname,hostname))
+	io.WriteString(w, fmt.Sprintf("<html><head><title>%s</title></head><body><h1>You hit: %s<br />Version:%s</h1></body></html>",hostname,hostname,VERSION))
+	fmt.Println(time.Now().UTC(),"/")
 }
 
 func main() {
 	http.HandleFunc("/", responder)
-	http.ListenAndServe(":8000",nil)	
+	http.ListenAndServe(":8000",nil)
 }
